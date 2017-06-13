0e298dbc46641f87be22c2fe5b04533190fe3c26
Tmairink <tmairink@hotmail.com>
2017-06-13 20:10:09 -0300

    InParce completo

---
+++ exercicos/3-MeuIntParce/3-MeuIntParce/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_MeuIntParce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mult = 1, n=0, i=0;
+            string s;
+            s = Console.ReadLine();
+            for (n = s.Length - 1; n >= 0; n--)
+            {
+                i += (s[n] - 48) * mult;
+                mult = mult * 10;
+            }
+            Console.WriteLine(i);
+
+
+
+        }
+    }
+}
