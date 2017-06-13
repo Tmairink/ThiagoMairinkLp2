aac34f669f47957d2bc7fa734dad2baf92e8dfc3
Tmairink <tmairink@hotmail.com>
2017-06-13 20:29:11 -0300

    Contagem De Vogais Completo

---
+++ exercicos/ContagemDeVogais/ContagemDeVogais/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContagemDeVogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra;
+            letra = Console.ReadLine().ToUpper();
+            int qnt = 0;
+
+            for (int i = 0; i < letra.Length; i++)
+            {
+                if (letra[i] == 'A' || letra[i] == 'E' || letra[i] == 'I' || letra[i] == 'O' || letra[i] == 'U')
+                {
+                    qnt++;
+                }
+            }
+
+            Console.WriteLine("A frase possui {0} vogais", qnt);
+            Console.ReadKey();
+        }
+    }
+}
