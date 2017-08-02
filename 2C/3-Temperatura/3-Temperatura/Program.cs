23c4b1a06774fcba7f23b884e01b520f3b23572e
Thiago Mairink <tmairink@hotmail.com>
2017-08-02 14:42:40 -0300

    Incompleto

---
+++ 2C/3-Temperatura/3-Temperatura/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C = double.Parse(Console.ReadLine());
+            TemperaturaCelsius Temp = new TemperaturaCelsius();
+
+            Temp.Calke(C);
+            
+        }
+    }
+}
---
+++ 2C/3-Temperatura/3-Temperatura/TemperaturaCelsius.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class TemperaturaCelsius
+    {
+        double F;
+
+        public double Calke(double C);
+        {
+            F = ((9*C)+32)/5;
+
+            return F;
+        }
+    }
+}
