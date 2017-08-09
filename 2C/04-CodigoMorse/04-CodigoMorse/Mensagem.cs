c833c2a5750347b761d50fbe9004ad8f712a5763
Thiago Mairink <tmairink@hotmail.com>
2017-08-09 15:52:45 -0300

    incompleto

---
+++ 2C/04-CodigoMorse/04-CodigoMorse/Mensagem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CodigoMorse
+{
+    class Mensagem
+    {
+        public string usuario;
+        public string retorno;
+        public string retorno2;
+        public string Som(string usuario)
+        {
+            for (int i = 0; i < usuario.Length; i++)
+            {
+
+            }
+            return retorno;
+        }
+        
+        public string Morse(string usuario)
+        {
+           
+            for (int i=0; i < usuario.Length; i++)
+            {
+                this.usuario = usuario;
+                string[] tabela = new string[26] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", };
+               
+
+                
+            }
+            return retorno2;
+            
+
+        }   
+        
+    }
+}
---
+++ 2C/04-CodigoMorse/04-CodigoMorse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario = Console.ReadLine();
+            int num=int.Parse(Console.ReadLine());
+            Mensagem sms = new Mensagem();
+            if (num==1)
+            {
+                Console.WriteLine(usuario);
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine(sms.Som(usuario));
+            }
+            else if(num ==3)
+            {
+                Console.WriteLine(sms.Morse(usuario));
+            }
+            else
+            {
+                Console.WriteLine("saiu");
+            }
+            
+        }
+    }
+}
