b0406731ecef2ee9d9e7d2e4dd32bf4866d07a60
Luan <luanmpbarros@gmail.com>
2017-08-16 16:03:26 -0300

    Completo

---
+++ 2C/05-Produtos/05-Produtos/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Produto
+    {
+        private int id;
+        public int Id {
+            get { return id; } 
+        }
+
+        public double Preco { get; set; }
+
+        private int quantidade;
+        public int Quantidade { get { return quantidade; } }
+
+        public string Nome { get; set; }
+
+        public Produto(int id,string Nome,double Preco)
+        {
+            this.id = id;
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.quantidade = 0;
+        }
+        public void Reposicao(int Quantidade)
+        {
+           quantidade += Quantidade;
+        }
+        public void Retirada(int Quantidade)
+        {
+            if (Quantidade <= quantidade)
+                quantidade -= Quantidade;
+            else
+                throw new Exception();
+                
+        }
+        public string Imprimir()
+        {
+            return String.Format(" Id {0}, Nome {1}, Preço R${2:0.00}", Id , Nome, Preco);
+        }
+
+    }
+}
---
+++ 2C/05-Produtos/05-Produtos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "sabonete Francis 60g", 1.49);
+            p.Reposicao(100);
+            p.Retirada(20);
+            //p.Retirada(90);
+
+            Console.WriteLine(p.Imprimir());
+        }
+    }
+}
