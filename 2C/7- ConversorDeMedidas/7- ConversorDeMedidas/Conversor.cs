1317a9d36072100fc4f56c28f1c1357f4566ccc1
Thiago <tmairink@hotmail.com>
2017-09-19 15:10:34 -0300

    Completo

--- 2C/7- ConversorDeMedidas/7- ConversorDeMedidas/Conversor.cs
+++ 2C/7- ConversorDeMedidas/7- ConversorDeMedidas/Conversor.cs
@@ -9,45 +9,117 @@ namespace _7__ConversorDeMedidas
     static class Conversor
     {
         static double R;
-        static double Kg;
-        static double G;
-        static double T;
-        static double Lbs;
-        static double valor;
-        public static double KgParaG (double x)
+        public static double KgParaG(double x)
         {
-            valor = x;
-            R= valor*1000;
+            R = x * 1000;
             return R;
         }
-        public static double KgParaT (double x)
+        public static double KgParaT(double x)
         {
-            valor = x;
-            R= valor/1000;
+            R = x / 1000;
             return R;
         }
-        public static double KgParaLbs (double x)
+        public static double KgParaLbs(double x)
         {
-            valor = x;
-            R = valor * 2.2046;
+            R = x * 2.2046;
             return R;
         }
         public static double GParaKg(double x)
         {
-            valor = x;
-            R = valor /1000;
+            R = x / 1000;
             return R;
         }
         public static double TParaKg(double x)
         {
-            valor = x;
-            R = valor * 1000;
+            
+            R = x * 1000;
             return R;
         }
         public static double LbsParaKg(double x)
         {
-            valor = x;
-            R = valor / 2.2046;
+            
+            R = x / 2.2046;
+            return R;
+        }
+
+        public static double CParaF(double x)
+        {
+            R = ((x - 32) / 9) * 5;
+            return R;
+        }
+        public static double CParaK(double x)
+        {
+            R = x + 273;
+            return R;
+        }
+        public static double FParaK(double x)
+        {
+            R = (x + 459) * 5 / 9;
+            return R;
+        }
+        public static double FParaC(double x)
+        {
+            R = (x - 32) * 5 / 9;
+            return R;
+        }
+        public static double KParaF(double x)
+        {
+            R = x * 9 / 5 - 459;
+            return R;
+        }
+        public static double KParaC(double x)
+        {
+            R = x - 273;
+            return R;
+        }
+        public static double MParaKm(double x)
+        {
+            R = x / 1000;
+            return R;
+        }
+        public static double KmParaM(double x)
+        {
+            R = x * 1000;
+            return R;
+        }
+        public static double KmParaMilha(double x)
+        {
+            R = x / 0.62;
+            return R;
+        }
+        public static double MilhaParaKm(double x)
+        {
+            R = x * 0.62;
+            return R;
+        }
+        public static double MParaPes(double x)
+        {
+            R = x * 3.28;
+            return R;
+        }
+        public static double PesParaM(double x)
+        {
+            R = x / 3.28;
+            return R;
+        }
+        public static double MParaPol(double x)
+        {
+            R = x * 39.37;
+            return R;
+        }
+        public static double PolParaM(double x)
+        {
+            R = x / 39.37;
+            return R;
+        }
+        public static double PolParaPes(double x)
+        {
+            R = x * 12 ;
+            return R;
+        }
+        public static double PesParaPol(double x)
+        {
+            R = x / 12;
             return R;
         }
 
--- 2C/7- ConversorDeMedidas/7- ConversorDeMedidas/Program.cs
+++ 2C/7- ConversorDeMedidas/7- ConversorDeMedidas/Program.cs
@@ -13,6 +13,7 @@ namespace _7__ConversorDeMedidas
             Console.WriteLine("Peso= 1 |Temperatura=2 |Distancia=3");
             int cv = Convert.ToInt32(Console.ReadLine());
             double id;
+            Console.Write("Valor a ser convertido:");
             id =Convert.ToDouble(Console.ReadLine());
             if (cv==1)
             {
@@ -41,6 +42,10 @@ namespace _7__ConversorDeMedidas
                 else if (ad==6)
                 {
                      Console.WriteLine(Conversor.LbsParaKg( id));
+                }
+                else
+                {
+                    throw new Exception();
                 }
             }
             else if(cv==2)
@@ -71,13 +76,62 @@ namespace _7__ConversorDeMedidas
                 {
                     Console.WriteLine(Conversor.KParaF(id));
                 }
+                else
+                {
+                    throw new Exception();
+                }
             }
             else if(cv==3)
             {
-                
-
+                Console.WriteLine("Km para M=1 || Km para Milha =2 || Milha para Km=3 || M para Km=4 || M para Pes =5 || M  para Polegadas =6 || Pes para M  =7|| Pes para Polegadas =8|| Polegada para M =9|| Polegadas para Pes  =10 ");
+                int ad = Convert.ToInt32(Console.ReadLine());
+                if (ad == 1)
+                {
+                    Console.WriteLine(Conversor.KmParaM(id));
+                }
+                else if (ad == 2)
+                {
+                    Console.WriteLine(Conversor.KmParaMilha(id));
+                }
+                else if (ad == 3)
+                {
+                    Console.WriteLine(Conversor.MilhaParaKm(id));
+                }
+                else if (ad == 4)
+                {
+                    Console.WriteLine(Conversor.MParaKm(id));
+                }
+                else if (ad == 5)
+                {
+                    Console.WriteLine(Conversor.MParaPes(id));
+                }
+                else if (ad == 6)
+                {
+                    Console.WriteLine(Conversor.MParaPol(id));
+                }
+                else if (ad == 7)
+                {
+                    Console.WriteLine(Conversor.PesParaM(id));
+                }
+                else if (ad == 8)
+                {
+                    Console.WriteLine(Conversor.PesParaPol(id));
+                }
+                else if (ad == 9)
+                {
+                    Console.WriteLine(Conversor.PolParaM(id));
+                }
+                else if (ad == 10)
+                {
+                    Console.WriteLine(Conversor.PolParaPes(id));
+                }
+                else
+                {
+                throw new Exception();
+                }
             }
-            else{
+            else
+            {
                 throw new Exception();
             }
             
