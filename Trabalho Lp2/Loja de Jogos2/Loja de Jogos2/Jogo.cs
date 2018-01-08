283cce6f683a7460af440e6c10d3adb85f743855
Thiago Mairink <tmairink@hotmail.com>
2018-01-08 13:00:50 -0200

    Ajuste na repeticao

--- Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Jogo.cs
+++ Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Jogo.cs
@@ -9,31 +9,22 @@ namespace Loja_de_Jogos2
 {
     class Jogo
     {
-        public string nome;
-        public string genero;
-        public string plataforma;
 
-        public int preço;
-        public int ano;
-
-
-        public void Cadastro(string Nome, int Preço, int Ano, string Genero, string Plataforma)
+        public void Cadastro(string Nome, int Preço, int Ano, string Genero, string Plataforma,int Quantidade)
         {
-            nome = Nome;
-            genero = Genero;
-            ano = Ano;
-            preço = Preço;
-            plataforma = Plataforma;
-
-            SqlConnection conexao = new SqlConnection("Data Source = localhost; Initial Catalog = LojaDeJogos; Integrated Security = SSPI;");
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = conexao;
-            cmd.CommandText = String.Format(@"Insert 
-                                            Into Jogo
-                                            Values('{0}', {1}, {2}, '{3}', '{4}');",nome, preço, ano, genero, plataforma);
-
+          
+                SqlConnection conexao = new SqlConnection("Data Source = localhost; Initial Catalog = LojaDeJogos; Integrated Security = SSPI;");
+                SqlCommand cmd = new SqlCommand();
+            
+                cmd.Connection = conexao;
+                cmd.CommandText = String.Format(@"Insert 
+                                                  Into Jogo(Nome, Preco, AnoDeLanc, Genero, Plataforma)
+                                                  Values('{0}', {1}, {2}, '{3}', '{4}');", Nome, Preço, Ano, Genero, Plataforma);
             cmd.Connection.Open();
-            cmd.ExecuteNonQuery();
+            for (int i = 1; i >= Quantidade; i++)
+            {               
+                cmd.ExecuteNonQuery();
+            }
             cmd.Connection.Close();
         }
     }
--- Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Program.cs
+++ Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Program.cs
@@ -11,31 +11,24 @@ namespace Loja_de_Jogos2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cadastro-1  compra-2 troca 3");
-            int x =Convert.ToInt32( Console.ReadLine());
+           Console.WriteLine("Cadastro-1  compra-2 troca 3");
+           int x =Convert.ToInt32( Console.ReadLine());
             
 
             string Nome, Genero, Plataforma;
-            int Preço, Ano, Quantidade;
-            Jogo caguei = new Jogo();
+            int Preço, Ano;
+            
+            int Quantidade = Convert.ToInt32(Console.ReadLine());
 
-            Quantidade = Convert.ToInt32(Console.ReadLine());
             if (x == 1)
             {
                 Preço = Convert.ToInt32(Console.ReadLine());
-                Ano = Convert.ToInt32(Console.ReadLine());            
+                Ano = Convert.ToInt32(Console.ReadLine());
                 Nome = Console.ReadLine();
                 Genero = Console.ReadLine();
                 Plataforma = Console.ReadLine();
-
-                int i = 1;
-                while ( i >= Quantidade)
-                {
-                    caguei.Cadastro(Nome, Preço, Ano, Genero, Plataforma);
-                    i++;
-                    
-                }
-                Console.WriteLine("foi");
+                Jogo caguei = new Jogo();
+                caguei.Cadastro(Nome, Preço, Ano, Genero, Plataforma,Quantidade);
             }
             else if (x == 2)
             {
@@ -46,9 +39,7 @@ namespace Loja_de_Jogos2
 
             }
             else
-            {
                 Console.WriteLine("erro");
-            }
         }
     }
 }
