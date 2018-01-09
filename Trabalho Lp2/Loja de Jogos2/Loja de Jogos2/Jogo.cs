d38c6f79d9384f0e86fca73b22dafc2fe01246d0
Thiago Mairink <tmairink@hotmail.com>
2018-01-09 00:07:36 -0200

    Compra quase terminada

--- Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Jogo.cs
+++ Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Jogo.cs
@@ -10,22 +10,57 @@ namespace Loja_de_Jogos2
     class Jogo
     {
 
-        public void Cadastro(string Nome, int Preço, int Ano, string Genero, string Plataforma,int Quantidade)
+        SqlConnection conexao = new SqlConnection("Data Source = localhost; Initial Catalog = LojaDeJogos; Integrated Security = SSPI;");
+        SqlCommand cmd = new SqlCommand();
+ 
+        public void Cadastro(string Nome, int Preço, int Ano, string Genero, string Plataforma)
         {
-          
-                SqlConnection conexao = new SqlConnection("Data Source = localhost; Initial Catalog = LojaDeJogos; Integrated Security = SSPI;");
-                SqlCommand cmd = new SqlCommand();
-            
-                cmd.Connection = conexao;
-                cmd.CommandText = String.Format(@"Insert 
+
+
+            cmd.Connection = conexao;
+            cmd.CommandText = String.Format(@"Insert 
                                                   Into Jogo(Nome, Preco, AnoDeLanc, Genero, Plataforma)
                                                   Values('{0}', {1}, {2}, '{3}', '{4}');", Nome, Preço, Ano, Genero, Plataforma);
             cmd.Connection.Open();
-            for (int i = 1; i >= Quantidade; i++)
-            {               
-                cmd.ExecuteNonQuery();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+        }
+        public void Compra(string Nome)
+        {
+            cmd.Connection = conexao;
+            cmd.CommandText = String.Format(@"SELECT DISTINCT Nome , Preco, AnoDeLanc, Genero, Plataforma 
+                                              FROM Jogo 
+                                              WHERE Nome ='{0}' ", Nome);
+
+            string nome;
+            int Preco = 0;
+            int Ano = 0;
+            string Genero;
+            string Plataforma;
+
+            cmd.Connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read() == true)
+                {
+                    nome = reader.GetString(0);
+                    Preco = reader.GetInt32(1);
+                    Ano = reader.GetInt32(2);
+                    Genero = reader.GetString(3);
+                    Plataforma = reader.GetString(4);
+
+                    Console.WriteLine("{0}, {1}, {2}, {3}, {4}", nome, Preco, Ano, Genero, Plataforma);
+                }
             }
             cmd.Connection.Close();
+
+            Console.WriteLine(Preco);
+            
+
+
+
+            //cmd.CommandText = String.Format(@"Delete From Jogo {0};", Nome);
         }
     }
 }
--- Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Program.cs
+++ Trabalho Lp2/Loja de Jogos2/Loja de Jogos2/Program.cs
@@ -13,26 +13,26 @@ namespace Loja_de_Jogos2
         {
            Console.WriteLine("Cadastro-1  compra-2 troca 3");
            int x =Convert.ToInt32( Console.ReadLine());
-            
-
-            string Nome, Genero, Plataforma;
-            int Preço, Ano;
-            
-            int Quantidade = Convert.ToInt32(Console.ReadLine());
-
+            Jogo caguei = new Jogo();
             if (x == 1)
             {
+                string Nome, Genero, Plataforma;
+                int Preço, Ano;
+                int Quantidade = Convert.ToInt32(Console.ReadLine());
                 Preço = Convert.ToInt32(Console.ReadLine());
                 Ano = Convert.ToInt32(Console.ReadLine());
                 Nome = Console.ReadLine();
                 Genero = Console.ReadLine();
                 Plataforma = Console.ReadLine();
-                Jogo caguei = new Jogo();
-                caguei.Cadastro(Nome, Preço, Ano, Genero, Plataforma,Quantidade);
+                for (int i = 1; i <= Quantidade; i++)
+                {
+                    caguei.Cadastro(Nome, Preço, Ano, Genero, Plataforma);
+                }
             }
             else if (x == 2)
             {
-
+                string Nome = Console.ReadLine();
+                caguei.Compra(Nome);
             }
             else if (x == 3)
             {
